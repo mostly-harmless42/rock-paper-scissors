760d3a747f9026b9cacd7ef73ccdad6b5e0c348e
mostly-harmless42 <wrgread1@gmail.com>
2022-02-11 22:21:28 -0800

    Edit rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,4 @@
 
-
-
 function computerPlay() {
   let play = Math.random() * 3;
   if (play < 1) {
@@ -10,4 +8,55 @@ function computerPlay() {
   } else return 'scissors';
 }
 
-console.log(computerPlay());
+function humanPlay() {
+  return window.prompt('type \'rock\', \'paper\', or \'scissors\': ').toLowerCase();
+}
+
+function round(human = humanPlay(), computer = computerPlay()) {
+  console.log(`${human} vs ${computer}`);
+
+  switch (human) {
+    case 'rock':
+      switch (computer) {
+        case 'rock': return 'tie';
+        case 'paper': return 'lose';
+        case 'scissors': return 'win';
+      }
+    case 'paper': 
+      switch (computer) {
+        case 'rock': return 'win';
+        case 'paper': return 'tie';
+        case 'scissors': return 'lose'
+      }
+    case 'scissors':
+      switch (computer) {
+        case 'rock': return 'lose';
+        case 'paper': return 'win';
+        case 'scissors': return 'tie';
+      }
+  }
+}
+
+
+function game(rounds) {
+  let record = [];
+  let wins = losses = ties = 0;
+
+  for (x = 0; x <= rounds; x++) {
+    let r = round()
+    console.log(r)
+    record.push(r)
+  }
+
+  for (x=0; x <= rounds; x++) {
+    switch (record[x]) {
+      case 'tie': {ties++}
+      case 'win': {wins++}
+      case 'loss': {losses++}
+    }
+  }
+}
+
+
+game(5)
+
